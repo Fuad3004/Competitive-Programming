f1ad91fab405e0a36103223992caa7b52d2434d3
Fuad Bin Ahmed <fuadbinahmed21@gmail.com>
2024-04-25 10:32:02 +0600

    Add files via upload

---
+++ BASICS/testcase_4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int tc=1;
+    while (1){
+        int n;
+        scanf("%d", &n);
+
+        if(n==0){
+            break;
+        }
+        printf("Case %d: %d\n", tc++, n);
+
+    }
+
+
+}
