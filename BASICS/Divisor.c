8c1af350999d65ccf95435f2d90daa18815d684d
Fuad Bin Ahmed <fuadbinahmed21@gmail.com>
2024-05-05 10:02:54 +0600

    Add files via upload

---
+++ BASICS/Divisor.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d",&n);
+
+    int i=1;
+    int cnt=0;
+
+    while(i*i <=n){
+        if(n%i==0){
+            cnt++;
+            if (i != n/i){
+                cnt++;
+            }
+
+        }
+        i++;
+    }
+    printf("%d",cnt);
+
+
+}
