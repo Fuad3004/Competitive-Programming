9731ce580d04604b67744777782026c2963eb1bb
Fuad Bin Ahmed <fuadbinahmed21@gmail.com>
2024-05-14 10:34:36 +0600

    Add files via upload

---
+++ BASICS/String1 Way too long word.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+
+    char s[123];
+
+    int t;
+    scanf("%d", &t) ;
+
+    while(t--){
+        scanf("%s",s);
+
+        int len=0;
+
+        while( s[len]!= '\0'){
+            len++;
+        }
+
+        if (len>10){
+            printf("%c%d%c", s[0], len-2, s[len-1]);
+        }
+        else{
+            printf("%s\n", s);
+        }
+    }
+}
